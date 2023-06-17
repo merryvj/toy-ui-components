9775efd8838659728d6ca35a4753459302a05005
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2023-06-17 13:35:12 -0400

    dial knob component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Tabs from "./components/tabs";
 import Card from "./components/card";
 import Button from "./components/button";
 import Drawer from "./components/drawer";
+import Dial from "./components/dial";
 
 export default function App() {
   const data = [
@@ -21,12 +22,16 @@ export default function App() {
   const [state, setState] = useState(0);
   return (
     <div>
-      <div className="sceneWrapper">
+      <div className="demoWrapper">
+        <Dial/>
+      </div>
+
+      {/* <div className="sceneWrapper">
         <Suspense>
         <Drawer />
           </Suspense>
-      </div>
-
+      </div> */}
+      
       <div className="demoWrapper">
         {data[0].items.map((item) => (
           <Card content={item.content} />
---
+++ src/components/dial/index.js
@@ -0,0 +1,54 @@
+import React, {useState} from "react";
+import "./style.css";
+import { motion } from "framer-motion";
+
+const numOptions = 3;
+
+export default function Check() {
+    const [checked, setChecked] = useState(false);
+    const [active, setActive] = useState(0);
+    const [direction, setDirection] = useState(1);
+
+    const handleClick = (e) => {
+        let nextActive = active + direction;
+        if(nextActive === numOptions - 1) setDirection(-1)
+        else if (nextActive === 0) setDirection(1);
+        setActive(active + direction);
+    }
+
+    return (
+        // <div className={checked ? "check-wrapper active" : "check-wrapper inactive"} onClick={() => setChecked(!checked)}>
+        //     <span className="check-inner"></span>
+        // </div>
+        <div className="dial-wrapper" onClick={handleClick}>
+            <motion.div className="dial"
+            animate={{rotate: -46 + (active * 46) - 1 + 'deg'}}
+            >
+
+            </motion.div>
+            <Indicators numOptions={numOptions} active={active}/>
+        </div>
+    )
+}
+
+const Indicators = ({numOptions, active}) => {
+    let indicators = [];
+    for (let i = 0; i < numOptions; i++) {
+        indicators.push(<Indicator index={i} active={active}/>)
+    }
+
+    return (
+        <div className="indicators">
+            {indicators}
+        </div>
+    );
+}
+
+const Indicator = ({active, index}) => {
+    //let rotation = (360 - (index * 45)) + 1 + "deg";
+    let isActive = index === active;
+
+    return(
+        <span className={`indicator deg-${index} ${isActive ? " on" :""}`}/>
+    )
+}
